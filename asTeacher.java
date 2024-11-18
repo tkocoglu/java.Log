47fd2d4365f73dd5bc752bb89bd870fb1606c9db
tkocoglu <tayyipkocoglu4@gmail.com>
2024-11-18 21:38:15 +0300

    Add files via upload

---
+++ asTeacher.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaproje;
+
+/**
+ *
+ * @author Tayyip Koçoğlu
+ */
+public class asTeacher {
+    
+    String name;
+    String mpno;
+    String branch;
+    
+    public asTeacher(String name, String mpno, String branch){
+        this.name = name;
+        this.mpno = mpno;
+        this.branch = branch;
+    }
+    
+}
